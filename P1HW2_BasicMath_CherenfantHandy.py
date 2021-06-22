25c99da2c4cba2c6f79212774e06e3801ae71075
cherenfh5854 <86271396+cherenfh5854@users.noreply.github.com>
2021-06-21 21:02:12 -0400

    Update P1HW2_BasicMath_CherenfantHandy.py

--- P1HW2_BasicMath_CherenfantHandy.py
+++ P1HW2_BasicMath_CherenfantHandy.py
@@ -3,9 +3,9 @@
 #CTI-110 P1HW2-Basic Math
 #Cherenfant Handy
 
-first_num= int(input())
-second_num= int(input())
+first_num= int(input('Please First Number Entered:'))
 print(first_num)
+second_num= int(input('Please Second Number Entered:'))
 print(second_num)
-print(first_num+second_num)
-print(first_num*second_num)
+print('Sum of both numbers:',first_num+second_num)
+print('Result of Multiplying the two numbers:',first_num*second_num)
