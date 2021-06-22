a5d09f941ada988527c3e10467d16b79e2aa44b0
cherenfh5854 <86271396+cherenfh5854@users.noreply.github.com>
2021-06-21 20:48:36 -0400

    Add files via upload

---
+++ P1HW2_BasicMath_CherenfantHandy.py
@@ -0,0 +1,11 @@
+#Basic math input and output
+#06/22/21
+#CTI-110 P1HW2-Basic Math
+#Cherenfant Handy
+
+first_num= int(input())
+second_num= int(input())
+print(first_num)
+print(second_num)
+print(first_num+second_num)
+print(first_num*second_num)
